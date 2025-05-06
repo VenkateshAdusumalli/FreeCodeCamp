695408d56c285913ae0aab7cf19c3b4d36f674c8
Venkatesh Adusumalli <venkateshadusumalli2007@gmail.com>
2025-05-06 11:57:02 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+from mean_var_std import calculate
+from test_module import test_calculate
+
+# Try the example input
+print("Example result:")
+print(calculate([0,1,2,3,4,5,6,7,8]))
+
+# Run unit tests
+print("\nRunning tests:")
+test_calculate()
